23d0620028b15185836b616d37baa7cd44fb2551
Paulo <Paulocpessoax@gmai.com>
2019-09-30 08:09:15 -0300

    Deep Leaning

--- part_8-deep-learning/artificial_neural_network/ann.py
+++ part_8-deep-learning/artificial_neural_network/ann.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('../../data_files/Churn_Modelling.csv')
+dataset = pd.read_csv('C:\git\Machine-Learning-A-Z-Udemy\data_files\Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
@@ -12,6 +12,7 @@ y = dataset.iloc[:, 13].values
 
 # Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
 labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
 labelencoder_X_2 = LabelEncoder()
@@ -22,10 +23,12 @@ X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
+
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
@@ -33,6 +36,7 @@ X_test = sc_X.transform(X_test)
 # ------- Part-2: Build the ANN --------
 
 # import keras library and packages
+import tensorflow
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
@@ -41,27 +45,45 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Adding the input layer and the first hidden layer
-layer_info = Dense(activation='relu', input_dim=11, kernel_initializer='uniform', units=6)
+layer_info = Dense(activation='relu',  # É o uso da função rectifier
+				   input_dim=11,  # Número de variáveis de entrada
+				   kernel_initializer='uniform',  # Inicializa os pesos de acordo com a distribuição uniforme
+				   units=6)  # units é o numero de neurons da hidden layer.
+# Uma regra básica é usar "(Variaveis de entrada + Variaveis de saida) / 2
 classifier.add(layer_info)
 
-# Adding second hidden layer
+# Adding second hidden layer, não precisa do parametro input_dim pois só no primeiro precisa.
 layer_info = Dense(activation='relu', kernel_initializer='uniform', units=6)
 classifier.add(layer_info)
 
 # Adding output layer
-layer_info = Dense(activation='sigmoid', kernel_initializer='uniform', units=1)
+layer_info = Dense(activation='sigmoid',  # Porque é a camada de caída, se tivesse 3 saidas ou mais seria softmax
+				   kernel_initializer='uniform',
+				   units=1)
 classifier.add(layer_info)
 
 # Compiling the ANN
-classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+classifier.compile(optimizer='adam',  # É um stochastic gradient descend usado para descobrir os pesos.
+				   loss='binary_crossentropy',  # Para calcular o cost function, essa função é comum em sigmoids.
+				   metrics=['accuracy'])  # A metrica para verificar a performance do modelo.
 
+import time
+start = time.time()
 # Fitting the ANN to the training set
-classifier.fit(X_train, y_train, batch_size=10, nb_epoch=100)
-
+classifier.fit(X_train, y_train,
+			   batch_size=10,  # Número de observações antes que se atualize os weights
+			   nb_epoch=10)  # Quando todo training set passar pelo ANN, completa 1 epoch
+print (time.time()-start)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5)
 
 # Making the confusion Matrix
 from sklearn.metrics import confusion_matrix
+
 cm = confusion_matrix(y_test, y_pred)
+
+
+
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
